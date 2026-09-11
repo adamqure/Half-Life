57793472eb038ebd2b2d1137200b0c77e5279864
Adam Ure <adam@snifr.ai>
2026-09-11 18:59:03 -0400

    Initial Commit

---
+++ Half-LifeTests/Half_LifeTests.swift
@@ -0,0 +1,23 @@
+//  _______  __   __  ___   ___      ___      _______  __    _  _______ 
+// |       ||  | |  ||   | |   |    |   |    |   _   ||  |  | ||       |
+// |   _   ||  | |  ||   | |   |    |   |    |  |_|  ||   |_| ||   _   |
+// |  | |  ||  |_|  ||   | |   |    |   |    |       ||       ||  | |  |
+// |  |_|  ||       ||   | |   |___ |   |___ |       ||  _    ||  |_|  |
+// |      | |       ||   | |       ||       ||   _   || | |   ||      | 
+// |____||_||_______||___| |_______||_______||__| |__||_|  |__||____||_|
+//  
+// Half-LifeTests Half_LifeTests
+//
+
+import Testing
+@testable import Half_Life
+
+struct Half_LifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ Half-LifeUITests/Half_LifeUITestsLaunchTests.swift
@@ -0,0 +1,39 @@
+//  _______  __   __  ___   ___      ___      _______  __    _  _______ 
+// |       ||  | |  ||   | |   |    |   |    |   _   ||  |  | ||       |
+// |   _   ||  | |  ||   | |   |    |   |    |  |_|  ||   |_| ||   _   |
+// |  | |  ||  |_|  ||   | |   |    |   |    |       ||       ||  | |  |
+// |  |_|  ||       ||   | |   |___ |   |___ |       ||  _    ||  |_|  |
+// |      | |       ||   | |       ||       ||   _   || | |   ||      | 
+// |____||_||_______||___| |_______||_______||__| |__||_|  |__||____||_|
+//  
+// Half-LifeUITests Half_LifeUITestsLaunchTests
+//
+
+import XCTest
+
+final class Half_LifeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
